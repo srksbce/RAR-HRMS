9242df34aee0468ab8cb0943797f6b51867cf863
susmitha28 <saisusmitha28@gmail.com>
2022-12-12 16:35:27 +0530

    up

--- RARHRMS/src/app/superuser/superuser-routing.module.ts
+++ RARHRMS/src/app/superuser/superuser-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SuperuserComponent } from './superuser/superuser.component';
 import { SuperuserinfoComponent } from './superuserinfo/superuserinfo.component';
-import { SuperuserinvoiceComponent } from './superuserinvoice/superuserinvoice.component';
+
 import { SuperuserrequestsComponent } from './superuserrequests/superuserrequests.component';
 import { VisaupdateComponent } from './visaupdate/visaupdate.component';
 
@@ -10,10 +10,8 @@ const routes: Routes = [
   {path:'superuser',component:SuperuserComponent},
   {path:'superuserinfo',component:SuperuserinfoComponent},
   {path:'superuserrequests',component:SuperuserrequestsComponent},
-  {path:'superuserinvoice',component:SuperuserinvoiceComponent},
-  {
-  path:'visaupdate',component:VisaupdateComponent
-  }
+
+  {path:'visaupdate',component:VisaupdateComponent}
   
 ];
 
