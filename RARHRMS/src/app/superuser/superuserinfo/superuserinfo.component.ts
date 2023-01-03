a27e0aac455226bb98257ab12b7fa0371190a83e
Santhoshkumarale <santhoshkumarale401@gmail.com>
2023-01-03 16:49:46 +0530

    chnages

--- RARHRMS/src/app/superuser/superuserinfo/superuserinfo.component.ts
+++ RARHRMS/src/app/superuser/superuserinfo/superuserinfo.component.ts
@@ -31,7 +31,7 @@ export class SuperuserinfoComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.empdata = JSON.parse(localStorage.getItem("personaldata") || '{}')
+    //this.empdata = JSON.parse(localStorage.getItem("personaldata") || '{}')
     console.log(this.empdata);
     this.getEducationData();
     this.getvisainformation();
@@ -42,10 +42,10 @@ export class SuperuserinfoComponent implements OnInit {
 
   getEducationData() {
     let sdata = {
-      "employeeid": parseInt(this.empdata.employeeid)
+      "employeeId": parseInt(this.empdata.employeeid)
     }
     console.log(sdata)
-    this.subscription = this.http.postdata("geteducation", sdata).subscribe({
+    this.subscription = this.http.postdata("Register", sdata).subscribe({
       next: (data: any) => {
         console.log(data);
         this.educationData = data
@@ -70,23 +70,99 @@ export class SuperuserinfoComponent implements OnInit {
   personaltab() {
     console.log(this.empdata)
      let sdata = {
-      "employeeid": parseInt(this.empdata.employeeid),
-      "empstatus": this.empdata.empstatus,
-      "firstname": this.empdata.firstname,
-      "lastname": this.empdata.lastname,
-      "fullname": this.empdata.fullname,
-      "dateofbirth": moment(this.empdata.dateofbirth),
-      "ssn": this.empdata.ssn,
-      "taxfilenumber": parseInt(this.empdata.taxfilenumber),
-      "addressline1": this.empdata.addressline1,
-      "addressline2": this.empdata.addressline2,
-      "city": this.empdata.city,
-      "state": this.empdata.state,
-      "country": this.empdata.country,
-      "zipcode": parseInt(this.empdata.zipcode),
-      "contactnumber": parseInt(this.empdata.contactnumber),
-      "emergencynumber": parseInt(this.empdata.emergencynumber),
-      "emailaddress": this.empdata.emailaddress,
+      "employeeId": this.empdata.employeeId,
+        "firstName": this.empdata.firstName,
+        "lastName": this.empdata.lastName,
+        "maritalStatus":this.empdata.maritalStatus,
+        "empstatus": this.empdata.empstatus,
+        "emailaddress": this.empdata.emailaddress,
+        "jobtitle": this.empdata.jobtitle,
+        "internalstaff": this.empdata.internalstaff,
+        "supervisor": this.empdata.supervisor,
+        "visaStatus": this.empdata.visaStatus,
+        "gender": this.empdata.gender,
+        "entity": this.empdata.entity,
+        "dob": this.empdata.dob,
+        "clientName": this.empdata.clientName,
+        "clientcode": this.empdata.clientcode,
+        "startdate": this.empdata.startdate,
+        "createdby": this.empdata.createdby,
+        "updatedby": this.empdata.updatedby,
+        "immigrationstatus": this.empdata.immigrationstatus,
+        "highestdegree": this.empdata.highestdegree,
+        "university": this.empdata.university,
+        "contactnumber": this.empdata.contactnumber,
+        "addressLine1": this.empdata.addressLine1,
+        "addressLine2": this.empdata.addressLine2,
+        "city": this.empdata.city,
+        "state": this.empdata.state,
+        "zipCode":this.empdata.zipCode,
+        "country":this.empdata.country,
+        "ssn": this.empdata.ssn,
+        "taxFileNumber": this.empdata.taxFileNumber,
+        "emergencynumber": this.empdata.emergencynumber,
+        "effectivedate": this.empdata.effectivedate,
+        "location": this.empdata.location,
+        "reportingTo":this.empdata.reportingTo,
+        "payschedule": this.empdata.payschedule,
+        "payType": this.empdata.payType,
+        "payrate":this.empdata.payrate,
+        "overtimeEligibility": this.empdata.overtimeEligibility,
+        "overtimerate": this.empdata.overtimerate,
+        "reason": this.empdata.reason,
+        "comments": this.empdata.comments,
+        "specialization": this.empdata.specialization,
+        "gpa": this.empdata.gpa,
+        "educationstartdate": this.empdata.educationenddate,
+        "educationenddate":this.empdata.educationenddate,
+        "visaTypeIssuingCountry": this.empdata.visaTypeIssuingCountry,
+        "issuedDate":this.empdata.issuedDate,
+        "expirationdate":this.empdata.expirationdate,
+        "status": this.empdata.status,
+        "employementstatus": this.empdata.employementstatus,
+        "jobEffectiveDate":this.empdata.jobEffectiveDate,
+        "compensationEffectiveDate":this.empdata.compensationEffectiveDate,
+        "id": this.empdata.id,
+        "title":this.empdata.title,
+        "nationalId": this.empdata.nationalId,
+        "profilePic": this.empdata.profilePic,
+        "secondaryLanguage":this.empdata.secondaryLanguage,
+        "addressId": this.empdata.addressId,
+        "workPhone":this.empdata.workPhone,
+        "mobilePhone":this.empdata.mobilePhone,
+        "workEmail": this.empdata.workEmail,
+        "homeEmail": this.empdata.homeEmail,
+        "degree": this.empdata.degree,
+        "notes": this.empdata.notes,
+        "bankName": this.empdata.bankName,
+        "accountNumber":this.empdata.accountNumber,
+        "userName": this.empdata.userName,
+        "password": this.empdata.password,
+        "confirmPassword": this.empdata.confirmPassword,
+        "ifscCode": this.empdata.ifscCode,
+        "proof": this.empdata.proof,
+        "dateOfJoining": this.empdata.dateOfJoining,
+        "probationEndDate": this.empdata.probationEndDate,
+        "probationStatus": this.empdata.probationStatus,
+        "noticePeriod": this.empdata.noticePeriod,
+        "backgroundCheckStatus": this.empdata.backgroundCheckStatus,
+        "department":this.empdata.department,
+        "businessArea": this.empdata.businessArea,
+        "compensationComments": this.empdata.compensationComments,
+        "frequency": this.empdata.frequency,
+        "amount": this.empdata.amount,
+        "emergencyName": this.empdata.emergencyName,
+        "relationShip": this.empdata.relationShip,
+        "emailId": this.empdata.emailId,
+        "mobileNumber":this.empdata.mobileNumber,
+        "projectName": this.empdata.projectName,
+        "projectStartDate": this.empdata.projectStartDate,
+        "projectEndDate": this.empdata.probationEndDate,
+        "termtypeoffboard": this.empdata.termtypeoffboard,
+        "termreasonoffboard": this.empdata.termreasonoffboard,
+        "eligibleforrehire": this.empdata.eligibleforrehire,
+        "lastworkingday": this.empdata.lastworkingday,
+        "jobRole": this.empdata.jobRole,
             }
 
     this.subscription = this.http.updatedata("empdata/", parseInt(this.empdata.employeeid),sdata).subscribe({
