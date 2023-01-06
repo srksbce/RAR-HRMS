6619996c6e8033ad3ab0833824e8a81386cfa684
Santhoshkumarale <santhoshkumarale401@gmail.com>
2023-01-06 11:33:13 +0530

    changes

--- RARHRMS/src/app/superuser/superuserrequests/superuser-model.ts
+++ RARHRMS/src/app/superuser/superuserrequests/superuser-model.ts
@@ -1,10 +1,27 @@
 export interface IExpensedata
 {
-    EmployeeID: number,
-    Expensedata: string,
-    ExpenseType:string,
+    id:any,
+    employeeID: number,
+    expensedate: string,
+    expenseType:string,
     amount: number,
     invoice:string,
+    status:string,
+    
+    attchment:any,
+
+
+
+}
+
+export interface Iinvoices
+{
+    id:number,
+    invoicenumber:number,
+    invoicedate:string,
+    invoiceType:string,
+    attchment:null,
+    amount: number,
     status:string,
 
 }
