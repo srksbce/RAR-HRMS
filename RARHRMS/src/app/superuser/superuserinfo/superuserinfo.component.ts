cb7df426bc5d0465dfef2c165219a83fd9f34ba4
Santhoshkumarale <santhoshkumarale401@gmail.com>
2023-01-09 09:32:33 +0530

    changes

--- RARHRMS/src/app/superuser/superuserinfo/superuserinfo.component.ts
+++ RARHRMS/src/app/superuser/superuserinfo/superuserinfo.component.ts
@@ -16,6 +16,8 @@ export class SuperuserinfoComponent implements OnInit {
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   subscription !: Subscription;
   educationData = {} as any;
+  JOB = {} as any;
+  getjob= {} as Ijob;
   visaData = [] as any;
   PERSONAL = {} as Ipersonaldata;
  
