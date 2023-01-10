97547ec4269ef1f11bd1d4216f24426192729e55
channupraveen <111118593+channupraveen@users.noreply.github.com>
2023-01-10 21:22:25 +0530

    update

--- RARHRMS/src/app/superuser/add-new-invoices/add-new-invoices.component.ts
+++ RARHRMS/src/app/superuser/add-new-invoices/add-new-invoices.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, NgForm, Validators } from '@angular/forms';
-import { Inewinv} from './add-new-invoices-model';
+import { Inewinv } from './add-new-invoices-model';
 import { AngularFileUploaderModule } from "angular-file-uploader";
 import { Router, Routes } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -16,59 +16,59 @@ export class AddNewInvoicesComponent implements OnInit {
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   NEWINVOICES = {} as any
   afuConfig = {
-    maxSize : 30,
+    maxSize: 30,
     multiple: false,
     formatsAllowed: ".jpg,.png",
     uploadAPI: {
-      url:"https://example-file-upload-api"
+      url: "https://example-file-upload-api"
     }
-};
+  };
 
 
-empids ={} as any;
-subscription!: Subscription;
-  constructor(private http:HttpService,private router:Router) { }
+  empids = {} as any;
+  subscription!: Subscription;
+  constructor(private http: HttpService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  NewINVOICES(f:NgForm){
+  NewINVOICES(f: NgForm) {
     this.NEWINVOICES.status = "In Review"
-    let empdata = JSON.parse(localStorage.getItem('personaldata') ||'{}')
+    let empdata = JSON.parse(localStorage.getItem('personaldata') || '{}')
     this.NEWINVOICES.employeeId = empdata.employeeId
     console.log(this.NEWINVOICES);
 
-    this.subscription = this.http.postdata("Invoice",this.NEWINVOICES).subscribe({
+    this.subscription = this.http.postdata("Invoice", this.NEWINVOICES).subscribe({
       next: (data: any) => {
-     console.log(data);
-     if(data){
-      alert("Data Updated Successfully")
-      this.router.navigate(['/superuser/superuserrequests'])
-      f.resetForm()
-     }
+        console.log(data);
+        if (data) {
+          alert("Data Updated Successfully")
+          this.router.navigate(['/superuser/superuserrequests'])
+          f.resetForm()
+        }
 
-     
+
       },
-  });
-}
+    });
+  }
 
 
-PostSavedInvoice(){
-  this.NEWINVOICES.status = "Draft"
-  let empdata = JSON.parse(localStorage.getItem('personaldata') ||'{}')
-  this.NEWINVOICES.employeeId = empdata.employeeId
-  console.log(this.NEWINVOICES);
-  this.subscription = this.http.postdata("Invoice",this.NEWINVOICES).subscribe({
-    next: (data: any) => {
-   console.log(data);
-   if(data){
-    alert("Data Updated Successfully")
-    this.router.navigate(['/superuser/superuserrequests'])
-    
-   }
+  PostSavedInvoice() {
+    this.NEWINVOICES.status = "Draft"
+    let empdata = JSON.parse(localStorage.getItem('personaldata') || '{}')
+    this.NEWINVOICES.employeeId = empdata.employeeId
+    console.log(this.NEWINVOICES);
+    this.subscription = this.http.postdata("Invoice", this.NEWINVOICES).subscribe({
+      next: (data: any) => {
+        console.log(data);
+        if (data) {
+          alert("Data Updated Successfully")
+          this.router.navigate(['/superuser/superuserrequests'])
 
-    },
-});
-}
-        
+        }
+
+      },
+    });
+  }
+
 }
