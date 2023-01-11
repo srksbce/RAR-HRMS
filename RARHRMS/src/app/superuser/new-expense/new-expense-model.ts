04b54971a7a2f4eebfe3a3fdea3922c80274e064
channupraveen <111118593+channupraveen@users.noreply.github.com>
2023-01-11 11:27:13 +0530

    update

--- RARHRMS/src/app/superuser/new-expense/new-expense-model.ts
+++ RARHRMS/src/app/superuser/new-expense/new-expense-model.ts
@@ -2,7 +2,7 @@ export interface Inewexpense{
     employeeId: any;
     Expensetype:string,
     Amount:number,
-    Datetime:number,
+    expensedate:number,
     Description:string,
     File:string,
     status:string
